72bcaeb0b09683f1c38ae3b38fe689c17b6b3a00
alicja <alicja.charuza@gmail.com>
2025-03-18 18:45:04 +0100

    add ability to choose model in experiments

--- experiments.py
+++ experiments.py
@@ -5,117 +5,168 @@ import numpy as np
 import pandas as pd
 import torch
 
+# Fake implementation of AlexNet so that the code runs without errors
+# Remove this when you implement AlexNet
+class AlexNet:
+    pass
 
-def default_parameters(lr, max_epochs, seed):
+
+def default_parameters(model, lr, max_epochs, seed):
     '''
     Default parameters for training
     '''
-    cnn = LeNet5(dropout_rate=0, init_type='random')
+    if model == "lenet5":
+        cnn = LeNet5(dropout_rate=0, init_type='random')
+    elif model == "alexnet":
+        cnn = AlexNet(dropout_rate=0, init_type='random')
+    else:
+        raise ValueError("Invalid model name")
     loader_creator = DataLoaderCreator(seed)
     train_loader = loader_creator.create_data_loader('cinic-10/train', batch_size=128, transform=loader_creator.standard_transform, shuffle=True)
     val_loader = loader_creator.create_data_loader('cinic-10/valid', batch_size=128, transform=loader_creator.standard_transform, shuffle=False)
     train_losses, val_losses = train_model(cnn, train_loader, val_loader, max_epochs=max_epochs, learning_rate=lr, seed=seed)
-    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/constant_lr_{lr}_seed_{seed}.csv', index=False)
-    torch.save(cnn.state_dict(), f'saved_models/constant_lr_{lr}_seed_{seed}.pth')
+    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/{model}_constant_lr_{lr}_seed_{seed}.csv', index=False)
+    torch.save(cnn.state_dict(), f'saved_models/{model}_constant_lr_{lr}_seed_{seed}.pth')
 
-def learning_rate_reduce(lr, max_epochs, seed):
+def learning_rate_reduce(model, lr, max_epochs, seed):
     '''
     Reducing learning rate when reaching a plateau
     '''
-    cnn = LeNet5(dropout_rate=0, init_type='random')
+    if model == "lenet5":
+        cnn = LeNet5(dropout_rate=0, init_type='random')
+    elif model == "alexnet":
+        cnn = AlexNet(dropout_rate=0, init_type='random')
+    else:
+        raise ValueError("Invalid model name")
     loader_creator = DataLoaderCreator(seed)
     train_loader = loader_creator.create_data_loader('cinic-10/train', batch_size=128, transform=loader_creator.standard_transform, shuffle=True)
     val_loader = loader_creator.create_data_loader('cinic-10/valid', batch_size=128, transform=loader_creator.standard_transform, shuffle=False)
     train_losses, val_losses = train_model(cnn, train_loader, val_loader, max_epochs=max_epochs, learning_rate=lr, lr_scheduling=True, seed=seed)
-    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/lr_reduce_{lr}_seed_{seed}.csv', index=False)
-    torch.save(cnn.state_dict(), f'saved_models/lr_reduce_{lr}_seed_{seed}.pth')
+    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/{model}_lr_reduce_{lr}_seed_{seed}.csv', index=False)
+    torch.save(cnn.state_dict(), f'saved_models/{model}_lr_reduce_{lr}_seed_{seed}.pth')
 
-def he_weight_init(lr, max_epochs, seed):
+def he_weight_init(model, lr, max_epochs, seed):
     '''
     He weight initialization
     '''
-    cnn = LeNet5(dropout_rate=0, init_type='he')
+    if model == "lenet5":
+        cnn = LeNet5(dropout_rate=0, init_type='he')
+    elif model == "alexnet":
+        cnn = AlexNet(dropout_rate=0, init_type='he')
+    else:
+        raise ValueError("Invalid model name")
     loader_creator = DataLoaderCreator(seed)
     train_loader = loader_creator.create_data_loader('cinic-10/train', batch_size=128, transform=loader_creator.standard_transform, shuffle=True)
     val_loader = loader_creator.create_data_loader('cinic-10/valid', batch_size=128, transform=loader_creator.standard_transform, shuffle=False)
     train_losses, val_losses = train_model(cnn, train_loader, val_loader, max_epochs=max_epochs, learning_rate=lr, seed=seed)
-    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/he_init_seed_{seed}.csv', index=False)
-    torch.save(cnn.state_dict(), f'saved_models/he_init_seed_{seed}.pth')
+    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/{model}_he_init_seed_{seed}.csv', index=False)
+    torch.save(cnn.state_dict(), f'saved_models/{model}_he_init_seed_{seed}.pth')
 
-def dropout(lr, max_epochs, seed):
+def dropout(model, lr, max_epochs, seed):
     '''
     Dropout with 20% dropout rate
     '''
-    cnn = LeNet5(dropout_rate=0.2, init_type='random')
+    if model == "lenet5":
+        cnn = LeNet5(dropout_rate=0.2, init_type='random')
+    elif model == "alexnet":
+        cnn = AlexNet(dropout_rate=0.2, init_type='random')
+    else:
+        raise ValueError("Invalid model name")
     loader_creator = DataLoaderCreator(seed)
     train_loader = loader_creator.create_data_loader('cinic-10/train', batch_size=128, transform=loader_creator.standard_transform, shuffle=True)
     val_loader = loader_creator.create_data_loader('cinic-10/valid', batch_size=128, transform=loader_creator.standard_transform, shuffle=False)
     train_losses, val_losses = train_model(cnn, train_loader, val_loader, max_epochs=max_epochs, learning_rate=lr, seed=seed)
-    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/dropout_seed_{seed}.csv', index=False)
-    torch.save(cnn.state_dict(), f'saved_models/dropout_seed_{seed}.pth')
+    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/{model}_dropout_seed_{seed}.csv', index=False)
+    torch.save(cnn.state_dict(), f'saved_models/{model}_dropout_seed_{seed}.pth')
 
-def random_rotation(lr, max_epochs, seed):
+def random_rotation(model, lr, max_epochs, seed):
     '''
     Random rotation from -20 to 20 degrees
     '''
-    cnn = LeNet5(dropout_rate=0, init_type='random')
+    if model == "lenet5":
+        cnn = LeNet5(dropout_rate=0, init_type='random')
+    elif model == "alexnet":
+        cnn = AlexNet(dropout_rate=0, init_type='random')
+    else:
+        raise ValueError("Invalid model name")
     loader_creator = DataLoaderCreator(seed)
     train_loader = loader_creator.create_data_loader('cinic-10/train', batch_size=128, transform=loader_creator.random_rotation_transform, shuffle=True)
     val_loader = loader_creator.create_data_loader('cinic-10/valid', batch_size=128, transform=loader_creator.standard_transform, shuffle=False)
     train_losses, val_losses = train_model(cnn, train_loader, val_loader, max_epochs=max_epochs, learning_rate=lr, seed=seed)
-    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/random_rotation_seed_{seed}.csv', index=False)
-    torch.save(cnn.state_dict(), f'saved_models/random_rotation_seed_{seed}.pth')
+    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/{model}_random_rotation_seed_{seed}.csv', index=False)
+    torch.save(cnn.state_dict(), f'saved_models/{model}_random_rotation_seed_{seed}.pth')
 
-def random_flipping(lr, max_epochs, seed):
+def random_flipping(model, lr, max_epochs, seed):
     '''
     Random horizontal flipping
     '''
-    cnn = LeNet5(dropout_rate=0, init_type='random')
+    if model == "lenet5":
+        cnn = LeNet5(dropout_rate=0, init_type='random')
+    elif model == "alexnet":
+        cnn = AlexNet(dropout_rate=0, init_type='random')
+    else:
+        raise ValueError("Invalid model name")
     loader_creator = DataLoaderCreator(seed)
     train_loader = loader_creator.create_data_loader('cinic-10/train', batch_size=128, transform=loader_creator.random_flipping_transform, shuffle=True)
     val_loader = loader_creator.create_data_loader('cinic-10/valid', batch_size=128, transform=loader_creator.standard_transform, shuffle=False)
     train_losses, val_losses = train_model(cnn, train_loader, val_loader, max_epochs=max_epochs, learning_rate=lr, seed=seed)
-    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/random_flipping_seed_{seed}.csv', index=False)
-    torch.save(cnn.state_dict(), f'saved_models/random_flipping_seed_{seed}.pth')
+    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/{model}_random_flipping_seed_{seed}.csv', index=False)
+    torch.save(cnn.state_dict(), f'saved_models/{model}_random_flipping_seed_{seed}.pth')
 
-def random_blur(lr, max_epochs, seed):
+def random_blur(model, lr, max_epochs, seed):
     '''
     Random Gaussian blur with kernel size 3
     '''
-    cnn = LeNet5(dropout_rate=0, init_type='random')
+    if model == "lenet5":
+        cnn = LeNet5(dropout_rate=0, init_type='random')
+    elif model == "alexnet":
+        cnn = AlexNet(dropout_rate=0, init_type='random')
+    else:
+        raise ValueError("Invalid model name")
     loader_creator = DataLoaderCreator(seed)
     train_loader = loader_creator.create_data_loader('cinic-10/train', batch_size=128, transform=loader_creator.random_blur_transform, shuffle=True)
     val_loader = loader_creator.create_data_loader('cinic-10/valid', batch_size=128, transform=loader_creator.standard_transform, shuffle=False)
     train_losses, val_losses = train_model(cnn, train_loader, val_loader, max_epochs=max_epochs, learning_rate=lr, seed=seed)
-    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/random_blur_seed_{seed}.csv', index=False)
-    torch.save(cnn.state_dict(), f'saved_models/random_blur_seed_{seed}.pth')
+    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/{model}_random_blur_seed_{seed}.csv', index=False)
+    torch.save(cnn.state_dict(), f'saved_models/{model}_random_blur_seed_{seed}.pth')
 
-def random_all(lr, max_epochs, seed):
+def random_all(model, lr, max_epochs, seed):
     '''
     All data augmentation techniques combined
     '''
-    cnn = LeNet5(dropout_rate=0, init_type='random')
+    if model == "lenet5":
+        cnn = LeNet5(dropout_rate=0, init_type='random')
+    elif model == "alexnet":
+        cnn = AlexNet(dropout_rate=0, init_type='random')
+    else:
+        raise ValueError("Invalid model name")
     loader_creator = DataLoaderCreator(seed)
     train_loader = loader_creator.create_data_loader('cinic-10/train', batch_size=128, transform=loader_creator.random_all, shuffle=True)
     val_loader = loader_creator.create_data_loader('cinic-10/valid', batch_size=128, transform=loader_creator.standard_transform, shuffle=False)
     train_losses, val_losses = train_model(cnn, train_loader, val_loader, max_epochs=max_epochs, learning_rate=lr, seed=seed)
-    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/random_all_seed_{seed}.csv', index=False)
-    torch.save(cnn.state_dict(), f'saved_models/random_all_seed_{seed}.pth')
+    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/{model}_random_all_seed_{seed}.csv', index=False)
+    torch.save(cnn.state_dict(), f'saved_models/{model}_random_all_seed_{seed}.pth')
 
-def cut_mix(lr, max_epochs, seed):
-    cnn = LeNet5(dropout_rate=0, init_type='random')
+def cut_mix(model, lr, max_epochs, seed):
+    if model == "lenet5":
+        cnn = LeNet5(dropout_rate=0, init_type='random')
+    elif model == "alexnet":
+        cnn = AlexNet(dropout_rate=0, init_type='random')
+    else:
+        raise ValueError("Invalid model name")
     loader_creator = DataLoaderCreator(seed)
     train_loader = loader_creator.create_data_loader_cutmix('cinic-10/train', batch_size=128, transform=loader_creator.standard_transform, shuffle=True)
     val_loader = loader_creator.create_data_loader('cinic-10/valid', batch_size=128, transform=loader_creator.standard_transform, shuffle=False)
     train_losses, val_losses = train_model(cnn, train_loader, val_loader, max_epochs=max_epochs, learning_rate=lr, seed=seed)
-    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/cut_mix_seed_{seed}.csv', index=False)
-    torch.save(cnn.state_dict(), f'saved_models/cut_mix_seed_{seed}.pth')
+    pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses}).to_csv(f'saved_losses/{model}_cut_mix_seed_{seed}.csv', index=False)
+    torch.save(cnn.state_dict(), f'saved_models/{model}_cut_mix_seed_{seed}.pth')
 
 if __name__ == "__main__":
     # choose the learning rate, max epochs and seed
     # then choose the experiment you want to run
     # experiments will save history of train and val losses and model weights
+    model = "lenet5"
     lr = 0.01
     max_epochs = 50
     seed = 123
-    cut_mix(lr, max_epochs, seed)
+    cut_mix(model, lr, max_epochs, seed)
