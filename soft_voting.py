d514af1593bd191e7e6eba124fcb3adbfc1296fe
alicja <alicja.charuza@gmail.com>
2025-03-30 21:22:17 +0200

    fix soft voting (now fr)

--- soft_voting.py
+++ soft_voting.py
@@ -1,5 +1,6 @@
 import numpy as np
-import torch.nn as nn
+import torch
+import torch.nn.functional as F
 
 class SoftVotingClassifier:
 
@@ -12,16 +13,32 @@ class SoftVotingClassifier:
 
     def predict_proba(self, X):
         self.classifiers[0].eval()
-        first_proba = nn.Softmax(self.classifiers[0](X), dim=1)
+        first_proba = F.softmax(self.classifiers[0](X), dim=1).detach().numpy()
         weighted_probs = np.zeros_like(first_proba)
+        weighted_probs = weighted_probs + self.weights[0] * first_proba
 
-        for clf, weight in zip(self.classifiers, self.weights):
+        for clf, weight in zip(self.classifiers[1:], self.weights[1:]):
             clf.eval()
-            probs = nn.Softmax(clf(X), dim=1)
-            weighted_probs += weight * probs
+            probs = F.softmax(clf(X), dim=1).detach().numpy()
+            weighted_probs = weighted_probs + weight * probs
 
         return weighted_probs / len(self.classifiers)
     
     def predict(self, X):
         probs = self.predict_proba(X)
         return np.argmax(probs, axis=1)
+    
+
+if __name__ == "__main__":
+    from alexnet import AlexNet
+    from lenet5 import LeNet5
+
+    classifiers = [AlexNet(dropout_rate=0, init_type="random"), LeNet5(dropout_rate=0, init_type="random")]
+    weights = [0.5, 0.5]
+    soft_voting_classifier = SoftVotingClassifier(classifiers, weights)
+
+    # X CINIC-10 image
+    X = torch.randn(1, 3, 32, 32)  # Example input tensor
+
+    print(soft_voting_classifier.predict_proba(X))
+    print(soft_voting_classifier.predict(X))
